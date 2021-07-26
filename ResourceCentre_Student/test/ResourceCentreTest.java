c148af249c861d41136736414e840c79f32051e6
angel <angel@LAPTOP-D76J14MS>
2021-07-26 12:17:43 +0800

    Update add chromebook

--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -52,6 +52,21 @@ public class ResourceCentreTest {
 	public void testAddChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		// Item list is not null, so that can add a new item
+		assertNotNull("Test if there is valid Chrombook arraylist to add to", chromebookList);
+		
+		//Given an empty list, after adding 1 item, the size of the list is 1
+		ResourceCentre.addChromebook(chromebookList, cb1);
+		assertEquals("Test if that Chromebook arraylist size is 1?",1,chromebookList.size());
+		
+		//The item just added is the same as the first item of the list
+		assertSame("Test that Chrombook is added same as the 1st item of the list?", cb1, chromebookList.get(0));
+		
+		//Add another item. Test the size of the list is 2?
+		ResourceCentre.addChromebook(chromebookList, cb2);
+		assertEquals("Test that Chrombook arraylist size is 2?",2,chromebookList.size());
+		
+		
 	}
 	
 	@Test
@@ -82,6 +97,14 @@ public class ResourceCentreTest {
 	public void testRetrieveAllChromebook() {
 		//fail("Not yet implemented");
 		// write your code here
+		
+		//Test if Item list of not null but empty, so that can add a new item
+		assertNotNull("Test if there is valid Chrombook arraylist to add to", chromebookList);
+		
+		//test if the list of chromebook retrieved from the SourceCenter is empty
+			String allChromebook = ResourceCentre.retrieveAllChromebook(chromebookList);
+			String testOutput = "";
+			assertEquals("Check that ViewAllChromebooklist", testOutput, allChromebook);
 	}
 
 	@Test
